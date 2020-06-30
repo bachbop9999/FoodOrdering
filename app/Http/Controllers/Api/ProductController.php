d448a3ec4ccfd648068daea579dc5f75607b4e1f
bachbop9999 <bachbop9999@gmail.com>
2020-06-30 19:52:29 +0700

    fix

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,62 +5,24 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+
+    public function getUsers()
     {
-        $products = ['alo'=>'alo'];
         $temp = DB::table('users')->get();
         return response()->json($temp, 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function insertUser(Request $request)
     {
-        //
+        $input = $request->only('name', 'email', 'password');
+
+        DB::table('users')->insert(
+            ['name' => $input['name'], 'email' => $input['email'], 'password'=> $input['password']]
+        );
+        return response()->json('Insert successgully', 200);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('products', 'Api\ProductController@index');
+Route::get('users', 'Api\ProductController@getUsers');
+Route::post('insert', 'Api\ProductController@insertUser');
