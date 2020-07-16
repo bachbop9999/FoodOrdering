aa1cf73a023e213e974c01caa549f446fd37b8bb
bachbop9999 <bachbop9999@gmail.com>
2020-07-16 19:55:25 +0700

    table

---
+++ database/migrations/2020_07_16_113107_table_order.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TableOrder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('table_no');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
