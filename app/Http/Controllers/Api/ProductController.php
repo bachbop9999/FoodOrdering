00a9a4231fe87b0552221a42d53d80184c857220
bachbop9999 <bachbop9999@gmail.com>
2020-07-16 18:26:00 +0700

    fix

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -8,23 +8,58 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+
 class ProductController extends Controller
 {
-    public function getPopularProduct()
+    public function getPopularProduct(Request $request)
     {
-        $popularProduct = Product::orderBy('rating', 'desc')->get();
+        $input = $request->only('category_id');
+        $rules = [
+            'category_id' => 'required|integer|exists:categories,id'
+        ];
+        $validator = Validator::make($input, $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' =>  $validator->getMessageBag()
+            ]);
+        }
+        $popularProduct = Product::where('category_id', $input['category_id'])->orderBy('rating', 'desc')->get();
         return response()->json($popularProduct, Response::HTTP_OK);
     }
 
-    public function getNewProduct()
+    public function getNewProduct(Request $request)
     {
-        $newProduct = Product::orderBy('created_at', 'desc')->get();
+        
+        $input = $request->only('category_id');
+        $rules = [
+            'category_id' => 'required|integer|exists:categories,id'
+        ];
+        $validator = Validator::make($input, $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' =>  $validator->getMessageBag()
+            ]);
+        }
+        $newProduct = Product::where('category_id', $input['category_id'])->orderBy('created_at', 'desc')->get();
         return response()->json($newProduct, Response::HTTP_OK);
     }
 
-    public function getProductSortByPrice()
+    public function getProductSortByPrice(Request $request)
     {
-        $priceProduct = Product::orderBy('price')->get();
+        $input = $request->only('category_id');
+        $rules = [
+            'category_id' => 'required|integer|exists:categories,id'
+        ];
+        $validator = Validator::make($input, $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' =>  $validator->getMessageBag()
+            ]);
+        }
+        $priceProduct = Product::where('category_id', $input['category_id'])->get();
         return response()->json($priceProduct, Response::HTTP_OK);
     }
 
@@ -34,7 +69,7 @@ class ProductController extends Controller
         $rules = [
             'id' => 'required|integer|exists:products'
         ];
-        
+
         $validator = Validator::make($input, $rules);
         if ($validator->fails()) {
             return response()->json([
@@ -44,6 +79,5 @@ class ProductController extends Controller
         }
         $detailProduct = Product::find($input['id']);
         return response()->json($detailProduct, Response::HTTP_OK);
-
     }
 }
