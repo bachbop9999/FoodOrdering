ecb9ab74323439d48365e40aaf4860c180618663
bachbop9999 <bachbop9999@gmail.com>
2020-07-16 20:01:29 +0700

    schedule

---
+++ database/migrations/2020_07_16_121904_schedules.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Schedules extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order_id')->unsigned();
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->integer('table_id')->unsigned();
+            $table->foreign('table_id')->references('id')->on('table');
+            $table->date('date_order');
+            $table->time('time_order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
